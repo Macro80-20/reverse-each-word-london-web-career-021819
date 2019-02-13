9a140a8ff9a5453f574553a5dd02c2e3de7a50a6
fernando <fernando_lopezc@live.com>
2019-02-13 11:56:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 se ="hello there and how are you?"
 def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  fresh_array =new_array. collect {|word| word.reverse}
+
+  fresh_array = sentence.split(" ").collect {|word| word.reverse}
 fresh_array.join(" ")
 end
 
